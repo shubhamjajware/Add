72bd905840d32f1c6821a0e66aa5fd6ffcb24de3
shubham jajware <shubham.raj53@yahoo.in>
2020-06-22 13:09:37 +0100

    calculator added

---
+++ Calculator/src/com/calculator/bdmas/Add.java
@@ -0,0 +1,17 @@
+package com.calculator.bdmas;
+
+public class Add {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=(a+b);
+		System.out.println("Add :"+c);
+
+	}
+
+}
