f6bcfda5898b231a05b1639fe56880e1ef8d8a60
shubham jajware <shubham.raj53@yahoo.in>
2020-06-22 14:05:31 +0100

    new branch created

---
+++ Calculator/src/Multiplication.java
@@ -0,0 +1,14 @@
+
+public class Multiplication {
+	public static void main(String[] args){
+		int d,a=3,b=2;
+		int c=(a*b);
+		System.out.println("Multiplication value: "+c);
+	
+	for(int i=1;i<=10;i++){
+		 d=(c*i);
+	
+	System.out.println(d);
+	}
+	}
+}
