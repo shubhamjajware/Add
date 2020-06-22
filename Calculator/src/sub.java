baf70da2619f60f6bfe817b79b1af8233f1f956c
shubham jajware <shubham.raj53@yahoo.in>
2020-06-22 13:52:15 +0100

    Subtraction added

---
+++ Calculator/src/sub.java
@@ -0,0 +1,16 @@
+
+public class sub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=10;
+		int c=(a-b);
+		System.out.println("Sub: "+c);
+
+	}
+
+}
